4b1151848a52c1c2eb4ba0d54d2fe8d64acb366d
Manuel Duran <durbonca@gmail.com>
2022-01-18 00:05:31 -0300

    i like this swipper... work pretty well

--- src/containers/projectsContainer.jsx
+++ src/containers/projectsContainer.jsx
@@ -7,16 +7,30 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
+import SwiperCore, { Navigation } from 'swiper';
+SwiperCore.use([Navigation]);
+
 function ProjectsContainer() {
   return (
     <div className="px-5">
       <ProjectsFilter />
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
+        spaceBetween={10}
         navigation
-        pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
+        pagination={{ el: '.swiper-pagination', type: 'bullets', clickable: true }}
+        allowTouchMove={true}
+        breakpoints={{
+          1024: {
+            slidesPerView: 3,
+          },
+          500: {
+            slidesPerView: 2,
+          },
+          300: {
+            slidesPerView: 1,
+          },
+        }}
       >
         <SwiperSlide>
           <ProjectCard />

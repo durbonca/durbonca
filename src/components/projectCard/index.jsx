aa854094d1ad295067648766d3cb47aa6f421b18
Manuel Duran <durbonca@gmail.com>
2022-01-21 12:42:52 -0300

    fix redirect bug netlify

--- src/components/projectCard/index.jsx
+++ src/components/projectCard/index.jsx
@@ -8,7 +8,7 @@ import { ButtonStyled, AccordionStyled, AccordionDetailsStyled } from './styles/
 function ProjectCard({ project }) {
   const { gitUrl, url, description, title, imgUrl } = project;
   return (
-    <div className="flex flex-col border-solid m-3 p-6 rounded-xl shadow-lg blue-glassmorphism w-[300px]">
+    <div className="flex flex-col justify-around border-solid m-3 p-6 rounded-xl shadow-lg blue-glassmorphism min-h-[450px] w-[300px]">
       <h1 className="text-2xl text-white">{title}</h1>
       <img className="w-5/6 h-40 object-cover mx-auto my-2 rounded-2xl shadow-lg" src={imgUrl} />
       <p className="my-2 text-sm text-white">{description}</p>
